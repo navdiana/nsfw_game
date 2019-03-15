881df646827a5b8979c2192075dbc59aa89980d8
11 <drudolph914@gmail.com>
2019-03-15 16:25:30 -0400

    rm-ing swap file and adding collision

--- logic.js
+++ logic.js
@@ -13,6 +13,12 @@ window.onload = function(){
             this.x = random;
             this.y = 10;
             this.radius = 10;
+
+            this.width = this.radius;
+            this.height = this.radius;
+            this.cx = this.x - this.radius/2;
+            this.cy = this.y - this.radius/2;
+
             this.color = "green";
         }
         draw(ctx){
@@ -24,7 +30,6 @@ window.onload = function(){
         }
 
         updatePos(){
-            console.log("yeet");
             this.y += 3;
         }
     }
@@ -32,9 +37,15 @@ window.onload = function(){
     class GoodStuff{
         constructor(){
             let random = Math.floor((Math.random() * 635) + 5);
-            this.x = random;
+            this.x = canvas.width/2;
             this.y = 10;
+
             this.radius = 10;
+            this.width = this.radius;
+            this.height = this.radius;
+            this.cx = this.x - this.radius/2;
+            this.cy = this.y - this.radius/2;
+
             this.color = "red";
         }
         draw(ctx){
@@ -53,7 +64,12 @@ window.onload = function(){
         constructor(x, y){
             this.x = x;
             this.y = y;
+
             this.radius = 10;
+            this.width = this.radius;
+            this.height = this.radius;
+            this.cx = this.x - this.radius/2;
+            this.cy = this.y - this.radius/2;
             this.color = "#F0F0F0";
         }
 
@@ -80,6 +96,7 @@ window.onload = function(){
             this.currTrash = 0;
             this.goodies = [];
             this.trashs = [];
+            this.debug = 0
         }
         spawnGoodies(){
             if(this.currGoodies<this.maxGoodies){
@@ -101,6 +118,7 @@ window.onload = function(){
                 this.trashs[i].draw(ctx);
             }
         }
+
         update(){
             for(let i = 0; i<this.currTrash;i++){
                 this.trashs[i].updatePos();
@@ -118,10 +136,37 @@ window.onload = function(){
             }
         }
 
+        collision(cursor){
+            for(let i=0; i<this.currGoodies; i++){
+                let goody = this.goodies[i];
+                if(this.debug < 200){
+                    console.log(this.debug);
+                    console.log(cursor.cx < goody.cx + goody.width);
+                    console.log(cursor.cx + cursor.width > goody.cx);
+                    console.log(cursor.cy < goody.cy + goody.height);
+                    console.log(cursor.cy + cursor.height > goody.cy);
+                    console.log('*****************************************');
+                    this.debug++;
+                }
+
+                if (cursor.cx < goody.cx + goody.width &&
+                    cursor.cx + cursor.width > goody.cx &&
+                    cursor.cy < goody.cy + goody.height &&
+                    cursor.cy + cursor.height > goody.cy) {
+
+                    console.log('hit');
+                }
+            }
+        }
+
     }
 
+
+    // Game objects
     var cursor = new Cursor(500, canvas.width/2);
     var gen = new Generator();
+
+
     // adding event listeners that get mouse cooridnates
     ctx.canvas.addEventListener('mousemove', function(event){
         MOUSE_X = event.clientX - ctx.canvas.offsetLeft;
@@ -133,10 +178,10 @@ window.onload = function(){
         ctx.clearRect(0,0,canvas.width, canvas.height);
         gen.spawnGoodies();
         gen.update();
-        cursor.draw(ctx);
+        gen.collision(cursor)
 
         gen.draw(ctx);
-
+        cursor.draw(ctx);
     }
 
     setInterval(run, 10);
